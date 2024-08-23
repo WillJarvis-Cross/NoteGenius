5e81c4378c552ff4c6836a26770660723c616325
wjarviscross <wjarviscross@gmail.com>
2024-08-23 14:15:41 -0400

    add storage

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Authenticator, Button } from '@aws-amplify/ui-react'
 import { Amplify } from 'aws-amplify'
 import styled from 'styled-components'
 import awsExports from '../aws-exports'
 import './SignInPage.css'
+import { getFileUrl } from '../utils/storageService'
 
 Amplify.configure(awsExports)
 
@@ -19,8 +20,22 @@ const CustomButton = styled(Button)`
     background-color: #0056b3
   }
 `
+const Logo = styled.img`
+  width: 150px; // Adjust the size of the logo
+  margin-bottom: 20px;
+`;
 
 function SignInPage() {
+  const [logoUrl, setLogoUrl] = useState(null)
+
+  useEffect(() => {
+    async function fetchLogo() {
+      const url = await getFileUrl('logo-color.png')
+      setLogoUrl(url)
+    }
+
+    fetchLogo()
+  }, [])
 
   return (
     <div className="sign-in-container">
@@ -29,10 +44,20 @@ function SignInPage() {
         components={{
           Button: CustomButton,
           SignIn: {
-            Header: () => <h1>Sign In</h1>,
+            Header: () => (
+              <div>
+                {logoUrl && <Logo src={logoUrl} alt="Logo" />}
+                <h1>Sign In</h1>
+              </div>
+            ),
           },
           SignUp: {
-            Header: () => <h1>Sign Up</h1>,
+            Header: () => (
+              <div>
+                {logoUrl && <Logo src={logoUrl} alt="Logo" />}
+                <h1>Sign Up</h1>
+              </div>
+            ),
           },
         }}
       >
@@ -46,5 +71,6 @@ function SignInPage() {
     </div>
   )
 }
+
 
 export default SignInPage
