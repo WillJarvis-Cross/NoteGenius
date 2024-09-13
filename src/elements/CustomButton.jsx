24c48173e8e665c1bcfa0d34db283d6e4496f121
wjarviscross <wjarviscross@gmail.com>
2024-09-13 18:08:49 -0400

    add class options

---
+++ src/elements/CustomButton.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${(props) => props.bgColor || '#007bff'};
+  color: ${(props) => props.textColor || '#ffffff'};
+  border: none;
+  border-radius: '4px';
+  padding: '10px 20px';
+  font-size: '16px';
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.hoverColor || '#0056b3'};
+  }
+`;
+
+const CustomButton = ({ children, onClick, bgColor, textColor, hoverColor }) => {
+  return (
+    <StyledButton
+      onClick={onClick}
+      bgColor={bgColor}
+      textColor={textColor}
+      hoverColor={hoverColor}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default CustomButton;
